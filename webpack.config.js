4dd9a8f1fd8c46617489804f7aff6f57277b0b23
Dmitry Pronin <aeoneluminaty2@gmail.com>
2017-08-14 21:16:11 +0300

    scss compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const webpack = require("webpack");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require("path");
 const nodeEnv = process.env.NODE_ENV || "development";
 const isProd = nodeEnv === "production";
+
 
 var config = {
     devtool: isProd ? "hidden-source-map" : "source-map",
     context: path.resolve("./src"),
     entry: {
-        app: "./main.tsx"
+        app: ["./main.tsx",  "./main.scss"]
     },
     output: {
         path: path.resolve("./dist"),
@@ -25,14 +27,23 @@ var config = {
                 exclude: ["node_modules"],
                 use: ["ts-loader", "source-map-loader"]
             },
-            { test: /\.html$/, loader: "html-loader" },
-            { test: /\.css$/, loaders: ["style-loader", "css-loader"] }
-        ]
+            {test: /\.html$/, loader: "html-loader"},
+            {test: /\.css$/, loaders: ["style-loader", "css-loader"]},
+            { // sass / scss loader for webpack
+                test: /\.(sass|scss)$/,
+                loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+            }
+        ],
+
     },
     resolve: {
-        extensions: [".ts", ".tsx", ".js"]
+        extensions: [".ts", ".tsx", ".js", ".scss"]
     },
     plugins: [
+        new ExtractTextPlugin({ // define where to save the file
+            filename: '[name].bundle.css',
+            allChunks: true,
+        }),
         new webpack.DefinePlugin({
             "process.env": {
                 // eslint-disable-line quote-props
@@ -45,8 +56,8 @@ var config = {
         //     filename: "vendor.bundle.js"
         // }),
         new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false },
-            output: { comments: false },
+            compress: {warnings: false},
+            output: {comments: false},
             sourceMap: true
         }),
         new webpack.LoaderOptionsPlugin({
