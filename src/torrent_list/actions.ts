f71cbe91a9052c874587b0e87daff927a108f82f
Dmitry Pronin <aeoneluminaty2@gmail.com>
2017-08-16 20:55:33 +0300

    start creating reducers

---
+++ src/torrent_list/actions.ts
@@ -0,0 +1,27 @@
+import {ActionTypes, BaseAction} from "./reducer";
+import {Torrent} from "../api/Models";
+
+function actionCreator(type:ActionTypes, payload:any):BaseAction<any> {
+    switch (type) {
+        case ActionTypes.FETCH_LIST_COMPLETE: {
+            return {
+                type: ActionTypes.FETCH_LIST_COMPLETE,
+                payload:payload
+            }
+        }
+    }
+
+    return {
+        type: type,
+        payload:payload
+    }
+}
+
+
+interface TorrentListActionCreator {
+    (type:ActionTypes.FETCH_LIST_COMPLETE, payload:Torrent[]):BaseAction<Torrent[]>;
+    (type:ActionTypes.FETCH_LIST, payload:void):BaseAction<void>;
+}
+
+
+export default (actionCreator as TorrentListActionCreator);
---
+++ src/torrent_list/reducer.ts
@@ -0,0 +1,48 @@
+import {Torrent} from "../api/Models";
+import {ActionsObservable} from "redux-observable";
+import {Observable} from "@reactivex/rxjs";
+import * as API from "../api/Api";
+import actionCreator from "./actions"
+
+interface TorrentListState {
+    torrents:Torrent[];
+}
+
+export const enum ActionTypes {
+    FETCH_LIST = 'torrent_list/FETCH_LIST',
+    FETCH_LIST_COMPLETE = 'torrent_list/FETCH_LIST_COMPLETE'
+}
+
+export interface BaseAction<T> {
+    type:ActionTypes;
+    payload:T;
+}
+
+
+export function torrentListLoadEpic(action$:ActionsObservable<BaseAction<any>>):Observable<BaseAction<any>> {
+    return (action$.ofType(ActionTypes.FETCH_LIST) as any as Observable<BaseAction<any>>)
+        .switchMap(() => {
+            return API.getTorrents((window as any)["SESSION"])
+        })
+        .map((torrents:Torrent[]) => {
+            return actionCreator(ActionTypes.FETCH_LIST_COMPLETE, torrents);
+        })
+
+}
+
+export function torrentListReducer(state:TorrentListState = { torrents: []}, action:BaseAction<any>):TorrentListState {
+
+    switch (action.type) {
+        case ActionTypes.FETCH_LIST: {
+            return state;
+        }
+
+        case ActionTypes.FETCH_LIST_COMPLETE: {
+            return {
+                torrents: action.payload
+            };
+        }
+    }
+
+    return state;
+}
